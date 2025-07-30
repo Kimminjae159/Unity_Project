010c847c2c8bcc2241d6e03c5c65320afda8ed7d
park2002js <park2002js@gmail.com>
2025-07-30 17:54:17 +0900

    Timer 임시 생성

---
+++ Assets/Scripts/Timer.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+using TMPro;
+using System;
+
+public class Timer : MonoBehaviour
+{
+    [SerializeField] TextMeshProUGUI timerText;
+    [SerializeField] float remainingTime;
+
+    void TimeCountdown()
+    {
+        int min = Mathf.FloorToInt(remainingTime / 60);
+        int sec = Mathf.FloorToInt(remainingTime % 60);
+        timerText.text = string.Format("{0:00}:{1:00}", min, sec);
+    }
+    void Update()
+    {
+        if (remainingTime > 0)
+        {
+            remainingTime -= Time.deltaTime;
+            if (remainingTime < 0)
+            {
+                remainingTime = 0;
+                timerText.color = Color.red;
+            }
+            TimeCountdown();
+        }
+    }
+}
