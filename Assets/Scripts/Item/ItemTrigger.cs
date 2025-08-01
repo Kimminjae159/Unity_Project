767321d52797081806247796f13147fd1a2ff4cf
park2002js <park2002js@gmail.com>
2025-08-01 19:05:20 +0900

    Level 1 수정
    Fog로 시야 제한
    안내견 프로토타입 생성
    Fog 제거 아이템 생성

--- Assets/Scripts/Item/ItemTrigger.cs
+++ Assets/Scripts/Item/ItemTrigger.cs
@@ -1,36 +1,64 @@
 using UnityEngine;
 using UnityEngine.Events;
 using System.Collections;
+using JetBrains.Annotations;
 
 public class ItemTrigger : MonoBehaviour
 {
     [Header("아이템 획득 시 실행할 기능")]
     public UnityEvent OnItemCollected;
 
+    [Header("비활성화 옵션")]
+    public bool ObjectDisable = false;
+    public bool FogDisable = false;
+
     [Header("비활성화할 오브젝트")]
-    public GameObject globalVolumeObject; // Inspector에서 할당
+    public GameObject DisableObject; // Inspector에서 할당
 
     [Header("비활성화 지속 시간 (초)")]
-    public float disableDuration = 3.0f;
+    public float DisableDuration = 3.0f;
+
+    private Renderer objectRenderer;
+
+    Color originalFogColor;       // 기존 Fog 색상 (옵션)
+    float originalFogDensity;     // 기존 Fog 밀도 (옵션)
+
+    void Start()
+    {
+        objectRenderer = GetComponent<Renderer>();
+        if (FogDisable)
+        {
+            originalFogColor = RenderSettings.fogColor;
+            originalFogDensity = RenderSettings.fogDensity;
+        }
+    }
 
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Player"))
         {
-            OnItemCollected?.Invoke();
-
-            if (globalVolumeObject != null)
-            {
-                StartCoroutine(DisableVolumeTemporarily());
-            }
-
-            Destroy(gameObject);
+            
+            StartCoroutine(DisableSomething());
         }
     }
-    private IEnumerator DisableVolumeTemporarily()
+    private IEnumerator DisableSomething()
     {
-        globalVolumeObject.SetActive(false);
-        yield return new WaitForSeconds(disableDuration);
-        globalVolumeObject.SetActive(true);
+        if (ObjectDisable)
+        {
+            DisableObject.SetActive(false);
+            objectRenderer.enabled = false;
+            yield return new WaitForSeconds(DisableDuration);
+            DisableObject.SetActive(true);
+        }
+        if (FogDisable)
+        {
+            RenderSettings.fog = false;
+            objectRenderer.enabled = false;
+            yield return new WaitForSeconds(DisableDuration);
+            RenderSettings.fog = true;
+            RenderSettings.fogColor = originalFogColor;
+            RenderSettings.fogDensity = originalFogDensity;
+        }
+        Destroy(gameObject);
     }
 }
