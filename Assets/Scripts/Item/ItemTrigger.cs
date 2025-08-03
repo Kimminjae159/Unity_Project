b2306da2e1740aa7941feb95ee75f9af27e70714
park2002js <park2002js@gmail.com>
2025-08-03 16:24:29 +0900

    아이템 획득 및 사용 기능 추가
    Fog 설정 아이템에 대해 Inventory에 넣고 사용가능 하도록 만들었습니다

--- Assets/Scripts/Item/ItemTrigger.cs
+++ Assets/Scripts/Item/ItemTrigger.cs
@@ -5,15 +5,15 @@ using JetBrains.Annotations;
 
 public class ItemTrigger : MonoBehaviour
 {
-    [Header("아이템 획득 시 실행할 기능")]
+    [Header("실행할 스크립트를 보유중인 아이템 설정")]
     public UnityEvent OnItemCollected;
 
-    [Header("비활성화 옵션")]
+    [Header("활성화할 옵션")]
     public bool ObjectDisable = false;
     public bool FogDisable = false;
 
-    [Header("비활성화할 오브젝트")]
-    public GameObject DisableObject; // Inspector에서 할당
+    [Header("비활성화할 외부 오브젝트")]
+    public GameObject ExternalObject; // Inspector에서 할당
 
     [Header("비활성화 지속 시간 (초)")]
     public float DisableDuration = 3.0f;
@@ -26,39 +26,53 @@ public class ItemTrigger : MonoBehaviour
     void Start()
     {
         objectRenderer = GetComponent<Renderer>();
-        if (FogDisable)
-        {
-            originalFogColor = RenderSettings.fogColor;
-            originalFogDensity = RenderSettings.fogDensity;
-        }
     }
 
     private void OnTriggerEnter(Collider other)
     {
+
         if (other.CompareTag("Player"))
         {
-            
-            StartCoroutine(DisableSomething());
+            OnItemCollected?.Invoke(); // OnItemCollected가 Null이 아니라면 할당된 함수를 실행
+            if (ObjectDisable)
+            {
+                StartCoroutine(DisableObjectFuc());
+            }
+            if (FogDisable)
+            {
+                StartCoroutine(DisableFogFuc());
+            }
         }
     }
-    private IEnumerator DisableSomething()
+
+    private IEnumerator DisableObjectFuc()
     {
-        if (ObjectDisable)
-        {
-            DisableObject.SetActive(false);
-            objectRenderer.enabled = false;
-            yield return new WaitForSeconds(DisableDuration);
-            DisableObject.SetActive(true);
-        }
-        if (FogDisable)
-        {
-            RenderSettings.fog = false;
-            objectRenderer.enabled = false;
-            yield return new WaitForSeconds(DisableDuration);
-            RenderSettings.fog = true;
-            RenderSettings.fogColor = originalFogColor;
-            RenderSettings.fogDensity = originalFogDensity;
-        }
+        ExternalObject.SetActive(false);
+        objectRenderer.enabled = false;
+        yield return new WaitForSeconds(DisableDuration);
+        ExternalObject.SetActive(true);
+
+        Destroy(gameObject);
+    }
+
+    // 외부에서 코루틴 함수 실행이 가능하도록 생성한 함수
+    public void ApplyFogFuc()
+    {
+        StartCoroutine(DisableFogFuc());
+    }
+
+    private IEnumerator DisableFogFuc()
+    {
+        originalFogColor = RenderSettings.fogColor;
+        originalFogDensity = RenderSettings.fogDensity;
+
+        RenderSettings.fog = false;
+        objectRenderer.enabled = false;
+        yield return new WaitForSeconds(DisableDuration);
+        RenderSettings.fog = true;
+        RenderSettings.fogColor = originalFogColor;
+        RenderSettings.fogDensity = originalFogDensity;
+
         Destroy(gameObject);
     }
 }
