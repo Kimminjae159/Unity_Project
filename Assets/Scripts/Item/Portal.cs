919d288824b58da5e66e588a9607bc11c46c0a0e
park2002js <park2002js@gmail.com>
2025-08-06 11:04:56 +0900

    조정

---
+++ Assets/Scripts/Item/Portal.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+public class Portal : MonoBehaviour
+{
+    public GameObject destination;
+
+    // Update is called once per frame
+    public void ApplyPortal()
+    {
+        destination.transform
+    }
+}
